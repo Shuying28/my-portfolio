589c5374ce7fe1bdeb84df796b430be409815107
Ong Shu Ying <ongshuyiing@gmail.com>
2025-04-06 23:55:39 +0800

    sy portfolio

---
+++ src/components/sections/Footer.jsx
@@ -0,0 +1,121 @@
+import React from "react";
+import styled from "styled-components";
+import { Bio } from "../../data/constants";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import { GitHub } from "@mui/icons-material";
+import { EmailRounded } from "@mui/icons-material";
+import { Phone } from "@mui/icons-material";
+
+const FooterContainer = styled.div`
+  width: 100%;
+  padding: 2rem 0;
+  display: flex;
+  justify-content: center;
+  position: relative;
+  z-index: 1;
+`;
+
+const FooterWrapper = styled.div`
+  width: 100%;
+  max-width: 1200px;
+  display: flex;
+  flex-direction: column;
+  gap: 14px;
+  align-items: center;
+  padding: 1rem;
+  color: ${({ theme }) => theme.text_primary};
+`;
+
+const Logo = styled.div`
+  font-weight: 600;
+  font-size: 20px;
+  color: ${({ theme }) => theme.primary};
+`;
+
+const Nav = styled.nav`
+  width: 100%;
+  max-width: 800px;
+  margin-top: 0.5rem;
+  display: flex;
+  flex-direction: row;
+  gap: 2rem;
+  justify-content: center;
+  @media (max-width: 768px) {
+    flex-wrap: wrap;
+    gap: 1rem;
+    justify-content: center;
+    text-align: center;
+    font-size: 12px;
+  }
+`;
+
+const NavLink = styled.a`
+  color: ${({ theme }) => theme.text_primary};
+  text-decoration: none;
+  font-size: 1.2rem;
+  transition: color 0.2s ease-in-out;
+  &:hover {
+    color: ${({ theme }) => theme.primary};
+  }
+  @media (max-width: 768px) {
+    font-size: 1rem;
+  }
+`;
+
+const SocialMediaIcons = styled.div`
+  display: flex;
+  margin-top: 1rem;
+`;
+const SocialMediaIcon = styled.div`
+  display: flex;
+  margin: 0 1rem;
+  font-size: 0.9rem;
+  text-align: center;
+  align-items: center;
+  color: ${({ theme }) => theme.text_primary};
+  transition: color 0.2s ease-in-out;
+  &:hover {
+    color: ${({ theme }) => theme.primary};
+  }
+`;
+
+const Copyright = styled.p`
+  margin-top: 1.5rem;
+  font-size: 0.9rem;
+  color: ${({ theme }) => theme.soft2};
+  text-align: center;
+`;
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <FooterWrapper>
+        <Logo>Shu Ying</Logo>
+        <Nav>
+          <NavLink href="#About">About</NavLink>
+          <NavLink href="#Skills">Skills</NavLink>
+          <NavLink href="#Experience">Experience</NavLink>
+          <NavLink href="#Projects">Projects</NavLink>
+          <NavLink href="#Education">Education</NavLink>
+        </Nav>
+        <SocialMediaIcons>
+          <SocialMediaIcon href={Bio.github} target="display">
+            <GitHub /> &nbsp; @Shuying28
+          </SocialMediaIcon>
+          <SocialMediaIcon href={Bio.linkedin} target="display">
+            <LinkedInIcon /> &nbsp; @Ong Shu Ying
+          </SocialMediaIcon>
+          <SocialMediaIcon href={`mailto:${Bio.email}`} target="display">
+            <EmailRounded /> &nbsp; {Bio.email}
+          </SocialMediaIcon>
+          <SocialMediaIcon href={`tel:${Bio.phone}`} target="display">
+            <Phone /> &nbsp; {Bio.phone}
+          </SocialMediaIcon>
+        </SocialMediaIcons>
+        <Copyright>&copy; 2025 Shu Ying. All rights reserved.</Copyright>
+      </FooterWrapper>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
---
+++ src/data/constants.js
@@ -0,0 +1,416 @@
+export const Bio = {
+  name: "Ong Shu Ying",
+  roles: [
+    "Full Stack Developer",
+    "Frontend Developer",
+    "Mobile Developer",
+    "WordPress Freelancer",
+  ],
+  description:
+    "I am a motivated software engineering graduate with a passion for developing innovative solutions in web and mobile development. I’m always open to learning, taking on new challenges, and growing through hands-on experience. I value collaboration, enjoy solving problems, and am committed to continuously improving my skills to contribute meaningfully to projects and teams.",
+  github: "https://github.com/Shuying28",
+  resume:
+    "https://drive.google.com/file/d/1EdtC9A93bXFT7_zKw5uDHrHGW870AWQU/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/ong-shu-ying/",
+  email: "ongshuyiing@gmail.com",
+  phone: "+601110979659",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "Angular Js",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNaaqOsJ43hX4TvXkWLeWwHIZ9a2RJiVajHw&s",
+      },
+      {
+        name: "Vue Js",
+        image:
+          "https://miro.medium.com/v2/resize:fit:400/0*K2ovtMil6X6nYrB5.png",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "TypeScript",
+        image:
+          "https://images.icon-icons.com/2415/PNG/512/typescript_plain_logo_icon_146316.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "Tailwind CSS",
+        image:
+          "https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      { name: "Node Js", image: "https://nodejs.org/static/images/logo.svg" },
+      {
+        name: "Express Js",
+        image:
+          "https://logowik.com/content/uploads/images/express-js1720895488.logowik.com.webp",
+      },
+      {
+        name: "Spring Boot",
+        image: "https://www.vectorlogo.zone/logos/springio/springio-icon.svg",
+      },
+      {
+        name: "Quarkus",
+        image:
+          "https://www.quarkus.io/assets/images/quarkus_logo_horizontal_rgb_reverse.svg",
+      },
+      {
+        name: "Laravel",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/9/9a/Laravel.svg",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MariaDB",
+        image: "https://www.vectorlogo.zone/logos/mariadb/mariadb-icon.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+    ],
+  },
+  {
+    title: "Mobile Development",
+    skills: [
+      {
+        name: "React Native",
+        image: "https://www.vectorlogo.zone/logos/reactjs/reactjs-icon.svg",
+      },
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      {
+        name: "XML",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBMw6_RdwKQ9bDFfnKDX1iwMl4bVJEvd9PP53XuIw&s",
+      },
+      {
+        name: "Android Studio",
+        image:
+          "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+      },
+      {
+        name: "XCode",
+        image:
+          "https://developer.apple.com/assets/elements/icons/xcode/xcode-256x256_2x.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "WordPress",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/9/98/WordPress_blue_logo.svg",
+      },
+      {
+        name: "Git",
+        image:
+          "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "IntelliJ IDEA",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/9/9c/IntelliJ_IDEA_Icon.svg",
+      },
+      {
+        name: "Render",
+        image:
+          "https://pbs.twimg.com/profile_images/1735429515541938176/zOO1N7Su_400x400.jpg",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://s3-alpha.figma.com/hub/file/1481185752/fa4cd070-6a79-4e1b-b079-8b9b76408595-cover.png",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://static-00.iconduck.com/assets.00/postman-icon-497x512-beb7sy75.png",
+      },
+      {
+        name: "Unity 2D",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZOQZppgSvzcQMjT4SNgWCC7tCHiy7AMdOgA&s",
+      },
+    ],
+  },
+  {
+    title: "Languages",
+    skills: [
+      {
+        name: "English",
+        image:
+          "https://upload.wikimedia.org/wikipedia/en/thumb/a/ae/Flag_of_the_United_Kingdom.svg/1200px-Flag_of_the_United_Kingdom.svg.png",
+      },
+      {
+        name: "Malay",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Flag_of_Malaysia.svg/1200px-Flag_of_Malaysia.svg.png",
+      },
+      {
+        name: "Chinese",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Flag_of_the_People%27s_Republic_of_China.svg/1200px-Flag_of_the_People%27s_Republic_of_China.svg.png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://cdn.theorg.com/a531c172-e167-4882-8b56-311fd2b4c2b2_thumb.jpg",
+    role: "Junior Software Engineer",
+    company: "OG Cloudtech",
+    date: "Feb 2025 - Present",
+    desc: "Designed and enhanced clients' back office front-end web pages using React (Ant Design). Developed and refined back-end API calls for back office sites with MariaDB, NodeJS, and NestJS. Wrapped mobile applications (Android & iOS) using Xcode and Android Studio.",
+    skills: ["React", "MariaDB", "NodeJS", "NestJS", "XCode", "Android Studio"],
+  },
+  {
+    id: 1,
+    role: "WordPress Freelancer",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiQqvP9mSAN_KNxZlbvD9VT-yl4Vf_PuT6Cw&s",
+    company: "Freelance",
+    date: "Dec 2024 - Present",
+    desc: "Developed clients' websites using WordPress, Elementor, and WooCommerce. Enhanced performance, added multilingual support, and customized themes, CSS, and plugins to meet specific needs.",
+    skills: ["WordPress", "Elementor", "WooCommerce", "CSS", "Plugins"],
+  },
+  {
+    id: 2,
+    role: "Frontend Developer Intern",
+    img: "https://ibsintelligence.com/wp-content/uploads/2024/02/Ant-International.jpg",
+    company: "Ant International",
+    date: "Jul 2024 - Oct 2024",
+    desc: "Involved in enterprise web application development using ReactJS (Ant Design) and NodeJS, along with React Unit Testing to ensure high-quality code. Collaborated closely with the product team to implement requirements, focusing on features that enhance user experience and functionality.",
+    skills: ["React", "NodeJS", "Unit Testing"],
+  },
+  {
+    id: 3,
+    role: "Full Stack Developer Intern",
+    img: "https://play-lh.googleusercontent.com/yiQOJ0lRrPpHOjkvm3RUxJIgbKspXCN3PSSPNzIn9aRFvS03sBgis3J4DhzDeIK8Dgo",
+    company: "IFAST Corporation Ltd",
+    date: "Aug 2023 - Feb 2024",
+    desc: "Involved in enterprise web application development using Angular, Spring Boot, Quarkus, SQL Developer, and Hibernate. Actively communicated with users to translate their requirements into system design and participated in the development life cycle, debugging, and enhancements.",
+    skills: ["Angular", "Spring Boot", "Quarkus", "SQL", "Hibernate"],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSS7Ih0oJcrQPkns16maH-rgvp7IMnkB5Howw&s",
+    school: "University of Malaya",
+    date: "2021 - 2025",
+    grade: "CGPA: 3.96 (Dean's Honors Award)",
+    degree: "Bachelor of Software Engineering",
+    desc: "Completed a Bachelor’s degree in Software Engineering with exceptional academic performance, earning the Dean's Honors Award. Gained extensive knowledge in software development, database management, and system design.",
+  },
+  {
+    id: 1,
+    img: "https://live.staticflickr.com/6122/5942107249_3ecbeeb5dc_m.jpg",
+    school: "Kolej Matrikulasi Negeri Sembilan (KMNS)",
+    date: "2020 - 2021",
+    grade: "CGPA: 4.00",
+    degree: "Foundation in Science (Module 1)",
+    desc: "Completed Foundation in Science with top academic results, preparing for advanced studies in software engineering. Achieved Band 4 in the Malaysian University English Test (MUET).",
+  },
+  {
+    id: 2,
+    img: "https://upload.wikimedia.org/wikipedia/ms/thumb/5/58/Sekolah_Menengah_Jenis_Kebangsaan_Notre_Dame_Convent.jpg/200px-Sekolah_Menengah_Jenis_Kebangsaan_Notre_Dame_Convent.jpg",
+    school: "SMJK Notre Dame Convent",
+    date: "2015 - 2019",
+    grade: "5A+ 5A (SPM)",
+    degree: "Sijil Pelajaran Malaysia (SPM)",
+    desc: "Excelled in high school with outstanding results in the SPM examinations, laying a strong foundation for further education in science and technology.",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "Infino",
+    date: "March 2025",
+    image: "infino.jpg",
+    description:
+      "Built a responsive web application using the WordPress Elementor low-code platform. Customized themes and CSS to match branding, and optimized performance using caching plugins to enhance loading speed.",
+    tags: ["Wordpress Elementor", "Cache Speed Optimization"],
+    category: "wordpress",
+    webapp: "https://infino.com.my/",
+  },
+  {
+    id: 1,
+    title: "Infino Gaming - Present",
+    date: "March 2025",
+    image: "infino-gaming.jpg",
+    description:
+      "Created a multilingual, mobile-friendly web application using WordPress Elementor. Customized themes and CSS. Supports both English and Chinese languages.",
+    tags: ["Wordpress Elementor", "Multilingual"],
+    category: "wordpress",
+    webapp: "https://infino.aladdinfamily.com/",
+  },
+  {
+    id: 2,
+    title: "Coway",
+    date: "Dec 2024 - Feb 2025",
+    image: "coway.jpg",
+    description:
+      "Developed a responsive e-commerce website using WordPress Elementor and WooCommerce. Customized themes and CSS, optimized performance with caching plugins, and integrated WooCommerce for efficient online shopping and product management.",
+    tags: ["Wordpress Elementor", "WooCommerce", "Cache Speed Optimization"],
+    category: "wordpress",
+    webapp: "https://cowayofficial.my/",
+  },
+  {
+    id: 3,
+    title: "Simple Contact Management System",
+    date: "Jan 2025 - Feb 2025",
+    image: "contact.jpg",
+    description:
+      "A simple contact management system built with Laravel, InertiaJS, Vue, and Vite. Utilized Vuetify for UI components and MySQL for database management. Implemented CRUD operations for managing contacts and integrated authentication features.",
+    tags: ["Laravel", "InertiaJS", "Vue", "Vite", "Vuetify", "MySQL"],
+    category: "web app",
+    github: "https://github.com/Shuying28/laravel-todo-app",
+    webapp: "https://laravel-inertia-vuetify.fly.dev/",
+  },
+  {
+    id: 4,
+    title: "Unite Mobile & Web Application",
+    date: "May 2024 - Jan 2025",
+    image: "unite.jpg",
+    description:
+      "Developed Malaysian social media mobile and web applications using React Native for mobile and React for web, with Express.js, Node.js, and MongoDB for the backend. Integrated a content filtering model and OpenAI moderation tools to analyze and filter Malaysian sensitive words. Implemented a post recommendation system and multi-language translation capabilities.",
+    tags: [
+      "React Native Expo",
+      "React",
+      "Express.js",
+      "Node.js",
+      "MongoDB",
+      "OpenAI",
+    ],
+    category: "mobile app",
+    github: "https://github.com/Kayxiii/Social-Media-for-Malaysian",
+  },
+  {
+    id: 5,
+    title: "Simple Angular Map Web Application",
+    date: "Feb 2025",
+    description:
+      "Developed an Angular application with a map interface to handle user-defined points, radius visualization, and automatic distance calculations(Haversine Formula). Integrated with Leaflet mapping library.",
+    image: "angular-map-application.jpg",
+    tags: ["Angular", "Typescript", "Leaflet", "Haversine Formula"],
+    category: "web app",
+    github: "https://github.com/Shuying28/angular-map-application",
+  },
+  {
+    id: 6,
+    title: "XMUM Ticket Booking Application",
+    date: "Sept 2024 - Oct 2024",
+    description:
+      "Collaborated with DanceClub of Xiamian University Malaysia to develop a ticket booking application for their annual concert. Utilized React for web development and Firebase for backend services.",
+    image: "bloom-in-motion.jpg",
+    tags: ["React", "Typescript", "Firebase"],
+    category: "web app",
+    github: "https://github.com/Shuying28/bloom-in-motion",
+    webapp: "https://bloom-in-motion.web.app/",
+  },
+  {
+    id: 7,
+    title: "Holland Food Web Application",
+    date: "May 2023 - Jul 2023",
+    description:
+      "Developed a responsive restaurant reservation website using HTML, CSS, Bootstrap, JavaScript, jQuery, PHP, and MySQL. Designed the GUI of the application using Figma.",
+    image: "holland-food.jpg",
+    tags: [
+      "HTML",
+      "CSS",
+      "Bootstrap",
+      "JavaScript",
+      "jQuery",
+      "PHP",
+      "MySQL",
+      "Figma",
+    ],
+    category: "web app",
+    github: "https://github.com/ongshuying/Holland-Food-App",
+  },
+  {
+    id: 8,
+    title: "Booking Luu Mobile Application",
+    date: "Nov 2022 - Jan 2023",
+    description:
+      "Developed a restaurant reservation mobile application using Android Studio with Java, XML, and Firebase. Integrated email notifications confirming reservation acceptance by the administrator and utilized the Google Map API to display the restaurant's location.",
+    image: "booking-luu.jpg",
+    tags: ["Java", "XML", "Android Studio", "Firebase", "Google Maps API"],
+    category: "mobile app",
+    github: "https://github.com/ongshuying/Booking-Luu-App",
+  },
+  {
+    id: 9,
+    title: "Vivere Mobile Application",
+    date: "Mar 2022 - Apr 2022",
+    description:
+      "Champion in International Level Varsity Hackathon 2022. Researched suicide-related issues and analyzed existing depression apps to identify shortcomings. Designed a unique prototype using Figma with highlighted features targeting suicide prevention and provided future recommendations for app improvement and expansion.",
+    image: "vhack.jpg",
+    tags: ["Figma", "Research", "Prototype Design"],
+    category: "mobile app",
+    prototype:
+      "https://www.figma.com/proto/X1z0FNDaA1BIAGyAdajDoa/Vivere?page-id=0%3A1&node-id=17-40&viewport=1402%2C838%2C0.76&t=tbTgBnhuTZGhCftG-1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=17%3A40&show-proto-sidebar=1",
+  },
+];
